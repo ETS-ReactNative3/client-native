977575ad3f7ab0c9aa59eab937f6dc0e4f0aab6a
Song Jaehak <master@hpi.cc>
2018-06-25 13:10:16 +0900

    refactor login api and add refreshToken api call

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,28 +1,48 @@
 import { RSAA } from 'redux-api-middleware';
 import { createAction } from "redux-actions"
 
-export const LOGIN = "USER/LOGIN";
-export const LOGIN_SUCCESS =  "USER/LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "USER/LOGIN_FAILURE";
-
 const API_URL = "https://develop.deepscent.io/";
 
 function createEndpoint(path) {
   return API_URL + path;
 }
 
-export const login = (email, pwd) => {
+function createApiAction({types, path, params}){
   return {
     [RSAA]: {
-      types: [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE],
-      endpoint: createEndpoint("auth/login"),
+      types,
+      endpoint: createEndpoint(path),
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
         'Referrer': 'arom-native',
         'Origin': 'arom-native'
       },
-      body: JSON.stringify({email, pwd}),
+      body: JSON.stringify(params),
     }
   };
 }
+
+export const LOGIN = "USER/LOGIN";
+export const LOGIN_SUCCESS =  "USER/LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "USER/LOGIN_FAILURE";
+
+export const login = (email, pwd) => {
+  return createApiAction({
+    types: [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE],
+    path: 'auth/login',
+    params: {email, pwd}
+  });
+};
+
+export const REFRESH_TOKEN = "USER/REFRESH_TOKEN";
+export const REFRESH_TOKEN_SUCCESS = "USER/REFRESH_TOKEN_SUCCESS";
+export const REFRESH_TOKEN_FAILURE = "USER/REFRESH_TOKEN_FAILURE";
+
+export const refreshToken = (refresh_token) => {
+  return createApiAction({
+    types: [REFRESH_TOKEN, REFRESH_TOKEN_SUCCESS, REFRESH_TOKEN_FAILURE],
+    path: 'auth/refresh_token',
+    params: { refresh_token }
+  });
+};
